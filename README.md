c4336c6edc8aa95de405bbb0eb3e2d9f03056d1b
Mihnea Simian <mihnea.simian@ing.ro>
2016-10-11 14:36:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Are-We-There-Yet App
+
+I started this to dig into Angular 2.
+
---
+++ app/event/add.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'event-add',
+  templateUrl: 'add.component.html'
+})
+export class EventAddComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
